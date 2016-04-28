b697a24bed0b9ad58cc8cbaf542ad4c47d84f1f0
princer007 <princer007@mail.ru>
2016-04-28 08:27:58 +0300

    Update ---- TEST.cs

--- ---- TEST.cs
+++ ---- TEST.cs
@@ -14,9 +14,14 @@ namespace ConsoleApplication10
             };
             PacketManager.RegisterPacket<UPacket>(1);
             UPacket var = PacketManager.Parse<UPacket>(packet);
-            Console.WriteLine(var.Bool);
             Console.WriteLine(var.Header);
-            Console.WriteLine(var.Something);
+            Console.WriteLine(var.Bool);
+            Console.WriteLine(var.Something+"\r\n");
+            foreach (byte b in packet)
+                Console.Write(b.ToString("x2") + " ");
+            Console.WriteLine();
+            foreach (byte b in PacketManager.Generate<UPacket>(var))
+                Console.Write(b.ToString("x2") + " ");
             Console.Read();
         }
     }
