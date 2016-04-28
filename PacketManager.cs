59a1b7a08ebde713872012c1c6cf5a2ca519d6f3
princer007 <princer007@mail.ru>
2016-04-28 08:27:34 +0300

    Update PacketManager.cs

--- PacketManager.cs
+++ PacketManager.cs
@@ -7,35 +7,37 @@ namespace RoyalPacketLib
 {
     public static class PacketManager
     {
-        static Dictionary<uint, Type> registeredPackets = new Dictionary<uint, Type>();
         static Dictionary<uint, PropertyInfo[]> packetsMetadata = new Dictionary<uint, PropertyInfo[]>();
         static Type packetType = typeof(Packet);
 
         static public void RegisterPacket<T>(uint header) where T : Packet
         {
-            Type t = typeof(T);
-            //if (!t.IsSubclassOf(packetType))
-                //throw new ArgumentException("T should inherit class Packet");
-            registeredPackets.Add(header, t);
-            packetsMetadata.Add(header, t.GetProperties().Where(x => x.CanWrite).ToArray());
+            Type t = typeof(T); // Does what it does
+            packetsMetadata.Add(header, t.GetProperties().Where(x => x.CanWrite && x.Name != "Header").ToArray()); // Add new Header-PropertyInfo[] element, not including Header 
         }
 
         static public T Parse<T>(byte[] content) where T:Packet
         {
-            T packet = (T)Activator.CreateInstance(typeof(T), content);
-            /*
-                Convert.ChangeType(packet, registeredPackets[packet.Header]);
-            */
+            T packet = (T)Activator.CreateInstance(typeof(T), content); //Creating new object inherited from Packet
             try
             {
                 foreach (var v in packetsMetadata[packet.Header])
-                    v.SetValue(packet, packet.ReadInternal(v.PropertyType), null);
+                    v.SetValue(packet, packet.ReadInternal(v.PropertyType), null); //Filling properties with data
             }
             catch (KeyNotFoundException)
-            { throw new ArgumentException("Current packet is not registered in PacketManager: " + packet.Header); }
-            packet.Flush();
+            { throw new ArgumentException("Current packet is not registered in PacketManager: " + packet.Header); } 
+            packet.Flush(); //Flushing "content" variable and setting pointer to zero
 
             return packet;
+        }
+        static public byte[] Generate<T>(T packet) where T:Packet
+        {
+            packet.Flush(); //Clearing packet content to replace it with something-something
+            packet.Write(packet.Header); //Since there is no header - workaround
+            foreach (var v in packetsMetadata[packet.Header])
+                packet.WriteInternal(v.GetValue(packet, null)); //Writing properties
+
+            return packet.Content;
         }
     }
 }
